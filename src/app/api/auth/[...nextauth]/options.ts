4bbea074984602464b056bec79a70ae21e101069
LikhithPalya <likhithpalya@gmail.com>
2025-01-16 12:31:26 +0530

    nextauth ecosystem all set!

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,78 @@
+import { NextAuthOptions } from "next-auth";
+import  CredentialsProvider  from "next-auth/providers/credentials";
+import bcrypt from 'bcryptjs';
+import { dbConnect } from "@/lib/dbConfig";
+import UserModel from "@/model/user.model";
+
+export const authOptions : NextAuthOptions ={
+    providers:[
+        CredentialsProvider({
+            id: "credentials",
+            name:"Credentials",
+            credentials: {
+                email: { label: "Email", type: "text", placeholder: "jondoe@gmail.com" },
+                password: { label: "Password", type: "password" }
+              },
+              async authorize(credentials:any): Promise<any>{
+                await dbConnect()
+                try {
+                    const user = await UserModel.findOne({
+                        $or:[ //FINDING USER DETAILS
+                            {email: credentials.identifier.email},
+                            {username: credentials.identifier.username}
+                        ]
+                    })
+                    if(!user){
+                        throw new Error("No user found with this email/username")
+                    }
+
+                    if (!user.isVerified) {
+                        throw new Error('Please verify your account before logging in');
+                    }
+
+                    const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password)
+
+                    if(isPasswordCorrect){
+                        return user
+                    }else{
+                        throw new Error("incorrect password")
+                    }
+
+                } catch (error:any) {
+                    console.log("unable to connect to db ",error);
+                    throw new Error(error)                    
+                }
+              }
+        })
+    ],
+    callbacks:{
+        async session({ session, token }) {
+            if(token){
+                session.user._id = token._id
+                session.user.isVerified = token.isVerified
+                session.user.isAcceptingMessages = token.isAcceptingMessages
+                session.user.username = token.username
+            }
+            return session
+          },
+          async jwt({ token, user }) { //WE GET THIS FROM THE RETURNED USER ABOVEtype
+            if(user){
+                token._id = user._id?.toString()
+                token.isVerified = user.isVerified
+                token.isAcceptingMessages = user.isAcceptingMessages;
+                token.username = user.username
+            }
+            return token
+          }    
+    },
+    pages:{
+        signIn: '/sign-in'
+    },
+    session:{
+        strategy:"jwt"
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+
+
+
+}
